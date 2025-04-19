95c03a3e70b167f6de27e76558f4df9abc11c1d8
Nhta22004335 <22004335@st.vlute.edu.vn>
2025-04-19 15:05:17 +0700

    aa

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@
 #include <DHT_U.h>
 #include <LiquidCrystal_I2C.h>
 
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/statement.h>  
+#include <cppconn/resultset.h>
+
 #define DHTTYPE DHT22  // Loại cảm biến: DHT22
 #define DHTPIN 4       // Chân DATA nối với GPIO4 nd và độ ẩm
 
@@ -104,6 +109,34 @@ void setup() {
   pinMode(LDR_PIN, INPUT);
   pinMode(POTE_PIN, INPUT);
   pinMode(PIR_PIN, INPUT);
+  
+  sql::mysql::MySQL_Driver* driver;
+  sql::Connection* conn;
+  sql::Statement* stmt;
+  sql::ResultSet* res;
+
+  // Lấy driver
+  driver = sql::mysql::get_mysql_driver_instance();
+  // Kết nối đến CSDL (user: root, password: , host: localhost)
+  conn = driver->connect("tcp://127.0.0.1:80", "root", "");
+
+  // Chọn database
+  conn->setSchema("iotgraden");
+
+  // Tạo statement và truy vấn
+  stmt = conn->createStatement();
+  res = stmt->executeQuery("SELECT * FROM nguoidung");
+
+  // In kết quả
+  while (res->next()) {
+    Serial.print( "Cột 1: ");
+    Serial.println(res->getString(1));
+  }
+
+  // Giải phóng bộ nhớ
+  delete res;
+  delete stmt;
+  delete conn;
 }
 
 void loop() {
@@ -123,7 +156,6 @@ void loop() {
   //CẢM BIẾN ÁNH SÁNG LDR
   float anhsang = analogRead(LDR_PIN);
   Serial.println(anhsang);
-  delay(5000);
   
   //CẢM BIẾN KHÍ GAS
   float khigas = analogRead(MQ2_PIN);  // Đọc giá trị từ MQ2 khí
@@ -188,6 +220,6 @@ void loop() {
   //   client.publish(MQTT_Topic_Gui, jsonData.c_str());
   //   tnd=0, tda=0, tkhi=0, tas=0;
   // }
-  //  delay(10000); 
+  delay(5000); 
 }
 
